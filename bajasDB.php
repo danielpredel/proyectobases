9ac137e1c981adbd0cd43f148fe547b2df357129
NeriU <118403833+NeriU@users.noreply.github.com>
2023-11-05 18:00:03 -0600

    delete actualizacion

--- bajasDB.php
+++ bajasDB.php
@@ -13,33 +13,38 @@
         $id = $_POST['id'];
         switch($tabla) {
             //Antes de borrar un registro, borramos las llaves foraneas a dicho registro
-            case "producto":
-                $bajasForaneas = "DELETE FROM comprar WHERE ProductoIdProducto=$id;";
+            case "Producto":
+                $bajasForaneas = "DELETE FROM Producto_Venta WHERE Producto_idProducto=$id;";
                 $conexion -> query($bajasForaneas);
-                $bajasForaneas = "DELETE FROM vender WHERE ProductoIdProducto=$id;";
+                $bajasForaneas = "DELETE FROM Producto_Proveedor WHERE Producto_idProducto=$id;";
                 $conexion -> query($bajasForaneas);
-                $bajasForaneas = "DELETE FROM suministrar WHERE ProductoIdProducto=$id;";
+                $bajasForaneas = "DELETE FROM Producto_Pedido WHERE Producto_idPedido=$id;";
                 $conexion -> query($bajasForaneas);
                 $atributoId = 'IdProducto';
                 break;
-            case "empleado":
-                $bajasForaneas = "DELETE FROM atender WHERE EmpleadoIdEmpleado=$id;";
+            case "Empleado":
+                $bajasForaneas = "DELETE FROM Venta WHERE Empleado_idEmpleado=$id;";
                 $conexion -> query($bajasForaneas);
-                $bajasForaneas = "DELETE FROM vender WHERE EmpleadoIdEmpleado=$id;";
-                $conexion -> query($bajasForaneas);
-                $atributoId = 'IdEmpleado';
+                $atributoId = 'idEmpleado';
                 break;
-            case "cliente":
-                $bajasForaneas = "DELETE FROM comprar WHERE ClienteIdCliente=$id;";
+            case "Cliente":
+                $bajasForaneas = "DELETE FROM Venta WHERE Cliente_idCliente=$id;";
                 $conexion -> query($bajasForaneas);
-                $bajasForaneas = "DELETE FROM atender WHERE ClienteIdCliente=$id;";
+                $bajasForaneas = "DELETE FROM Consultar WHERE Cliente_idCliente=$id;";
                 $conexion -> query($bajasForaneas);
-                $atributoId = 'IdCliente';
+                $atributoId = 'idCliente';
                 break;
-            case "proveedor":
-                $bajasForaneas = "DELETE FROM suministrar WHERE ProveedorIdProveedor=$id;";
+            case "Proveedor":
+                $bajasForaneas = "DELETE FROM Producto_Proveedor WHERE Proveedor_idProveedor=$id;";
                 $conexion -> query($bajasForaneas);
-                $atributoId = 'IdProveedor';
+                $bajasForaneas = "DELETE FROM Pedido WHERE Proveedor_idProveedor=$id;";
+                $conexion -> query($bajasForaneas);
+                $atributoId = 'idProveedor';
+                break;
+            case "Doctor":
+                $bajasForaneas = "DELETE FROM Consultar WHERE Doctor_idDoctor=$id;";
+                $conexion -> query($bajasForaneas);
+                $atributoId = 'idDoctor';
                 break;
         }
         $baja = "DELETE FROM $tabla WHERE $atributoId=$id;";
